e478bfce2a3f2ac837b45d328cfecdc1873c235a
AnnaLenaO <annalenao@me.com>
2024-09-23 14:16:38 +0200

    Try concept with example code

---
+++ src/main/java/com/example/javalab/Product.java
@@ -0,0 +1,4 @@
+package com.example.javalab;
+
+public record Product(String name, double rating) {
+}
---
+++ src/main/java/com/example/javalab/Products.java
@@ -0,0 +1,6 @@
+package com.example.javalab;
+
+import java.util.List;
+
+public record Products(List<Product> Product, String status) {
+}
