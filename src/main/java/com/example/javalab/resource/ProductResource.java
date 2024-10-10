caed81897d4a3f62ed3326a51a042b8885236aff
AnnaLenaO <annalenao@me.com>
2024-10-10 13:37:13 +0200

    ProductResource with all POST & GET.

--- src/main/java/com/example/javalab/resource/ProductResource.java
+++ src/main/java/com/example/javalab/resource/ProductResource.java
@@ -1,13 +1,18 @@
 package com.example.javalab.resource;
 
+import com.example.javalab.entities.Category;
+import com.example.javalab.entities.InputProductData;
 import com.example.javalab.entities.ProductList;
 import com.example.javalab.service.Warehouse;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
+
+import java.net.URI;
+import java.util.UUID;
 
 @Path("/")
 public class ProductResource {
@@ -25,11 +30,38 @@ public class ProductResource {
         this.warehouse = warehouse;
     }
 
+    @POST
+    @Path("/products")
+    @Consumes("application/json")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response createProduct(InputProductData product) {
+        warehouse.addNewProduct(product);
+//        return Response.created(uriInfo.getAbsolutePath()).build();
+        return Response.created(URI.create("/products")).build();
+    }
+
     @GET
     @Path("/products")
-    @Produces("application/json")
-//    @Produces(MediaType.APPLICATION.JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public ProductList allProducts() {
         return new ProductList(warehouse.getProductList());
+    }
+
+    @GET
+    @Path("/products/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getProduct(@PathParam("id") UUID id) {
+//        warehouse.getAProductForItsId(id);
+//        return Response.created(URI.create("/products/" + id)).build();
+        return Response.created(URI.create("/products/" + id))
+                .entity(warehouse.getAProductForItsId(id)).build();
+    }
+
+    @GET
+    @Path("/products/categories/{category}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getProductCategories(@PathParam("category") Category category) {
+        return Response.created(URI.create("/categories/" + category))
+                .entity(warehouse.getSortedProductsForACategory(category)).build();
     }
 }
